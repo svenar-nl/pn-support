3beb4544038c13101d328633b8108c0abd49f45c
itzJOHv <kaktusgame806@gmail.com>
2021-06-18 21:38:08 +0300

    Create stats.js

---
+++ src/commands/stats.js
@@ -0,0 +1,52 @@
+const { Command } = require('discord-akairo'),
+    os = require('os');
+
+class StatsCommand extends Command {
+  constructor() {
+    super('stats', {
+      category: 'Utility',
+      description: 'Get the bot usages.',
+      aliases: ['monitor', 'monit', 'statistics'],
+      cooldown: 3e3
+    });
+  };
+
+  async exec(msg) {
+    const sent = await msg.util.reply('📶 Getting info...');
+    return sent.edit([
+      `📈 **CPU Usage**: ${cpu}`,
+      `📊 **RAM Usage**: ${ram}`
+    ]);
+  };
+};
+
+const cpu = () => {
+    // Take the first CPU, considering every CPUs have the same specs
+    // and every NodeJS process only uses one at a time.
+    let cpus = os.cpus(),
+        cpu0 = cpus[0];
+
+    // Accumulate every CPU times values
+    const total = Object.values(cpu0.times).reduce((acc, tv) => acc + tv, 0);
+
+    // Normalize the one returned by process.cpuUsage() 
+    // (microseconds VS miliseconds)
+    let usage = process.cpuUsage(),
+        currentCPUUsage = (usage.user + usage.system) * 1000;
+
+    // Find out the percentage used for this specific CPU
+    const perc = currentCPUUsage / total * 100;
+
+    return `\`${perc}%\``;
+};
+
+const ram = () => {
+    let list = [];
+
+    Object.entries(process.memoryUsage())
+        .forEach(item => list.push(`${item[0]}: ${(item[1] / 1024 / 1024).toFixed(4)} MB`));
+    
+    return list.join('\n');
+};
+
+module.exports = StatsCommand;
