10b3cff31c83524b47eb86c1aadafbb222bd1c2c
Sven Arends <github@svenar.nl>
2021-06-18 21:19:11 +0200

    Improved cpu formatting & added stats alias

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -6,7 +6,7 @@ class StatsCommand extends Command {
     super('stats', {
       category: 'Utility',
       description: 'Get the bot usages.',
-      aliases: ['monitor', 'monit', 'statistics'],
+      aliases: ['monitor', 'monit', 'statistics', 'stats'],
       cooldown: 3e3
     });
   };
@@ -22,27 +22,27 @@ class StatsCommand extends Command {
 
 function cpu() {
     // Take the first CPU, considering every CPUs have the same specs
-    // and every NodeJS process only uses one at a time. 
+    // and every NodeJS process only uses one at a time.
     let cpus = os.cpus(),
         cpu0 = cpus[0];
 
     // Accumulate every CPU times values
     const total = Object.values(cpu0.times).reduce((acc, tv) => acc + tv, 0);
 
-    // Normalize the one returned by process.cpuUsage() 
+    // Normalize the one returned by process.cpuUsage()
     // (microseconds VS miliseconds)
     let usage = process.cpuUsage(),
         currentCPUUsage = (usage.user + usage.system) * 1000;
 
     // Find out the percentage used for this specific CPU
-    const perc = currentCPUUsage / total * 100;
+    const perc = (currentCPUUsage / total * 100).toFixed(2);
     let bar = "";
 
     for (var i = 0; i < 100; i+= 10) {
         bar += i < perc ? "▓" : "░";
     }
 
-    return `\n\`${perc}% [${bar}]\``;
+    return `\n\`${bar} ${perc}%\``;
 };
 
 function ram() {
@@ -50,7 +50,7 @@ function ram() {
 
     Object.entries(process.memoryUsage())
         .forEach(item => list.push(`${item[0]}: ${(item[1] / 1024 / 1024).toFixed(4)} MB`));
-    
+
     return `\`\`\`${list.join('\n')}\`\`\``;
 };
 
